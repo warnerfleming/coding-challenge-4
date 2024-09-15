c53410f352cfb694bec67a747bef24702cdbc302
warnerfleming <fleming14@usf.edu>
2024-09-15 16:36:06 -0400

    Initialize inventory with product objects

--- inventory management.js
+++ inventory management.js
@@ -1,1 +1,9 @@
+//Task 1
 
+const inventory = [
+        {name: 'Battery', price: 80, quantity: 20, lowstocklevel: 5},
+        {name: 'Battery', price: 80, quantity: 20, lowstocklevel: 5},
+        {name: 'Battery', price: 80, quantity: 20, lowstocklevel: 5},
+        {name: 'Battery', price: 80, quantity: 20, lowstocklevel: 5},
+        {name: 'Battery', price: 80, quantity: 20, lowstocklevel: 5}
+]
